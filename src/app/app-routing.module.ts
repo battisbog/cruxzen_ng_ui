04018b6d7979d9dfcc256b6ffd08df00a027b741
battisbog <ishkiwashka@gmail.com>
2023-10-16 10:52:38 +0530

    fg

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
+// Import the components
 import { DashboardComponent } from './dashboard/dashboard.component';
 
+// Define the routes
 const routes: Routes = [
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: 'dashboard', component: DashboardComponent },
+  {
+    path: '', 
+    redirectTo: '/dashboard', 
+    pathMatch: 'full'
+  },
+  {
+    path: 'dashboard', 
+    component: DashboardComponent
+  },
+  // Potentially more routes in the future...
 ];
 
 @NgModule({
