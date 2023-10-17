c1d4cee6093cc975470e7dd62a31fe8028bd5460
battisbog <ishkiwashka@gmail.com>
2023-10-17 09:22:04 +0530

    no change

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -17,19 +17,20 @@ export class DashboardComponent {
 
     onLoadData(event: boolean) {
         if (event) {
-            this.graphData = [
-                { month: 'January', val: 15 },
-                { month: 'February', val: 20 },
-                { month: 'March', val: 25 },
-                // ... other data
-            ];
+          this.graphData = [
+            { name: 'Jan', value: 120 },
+            { name: 'Feb', value: 150 },
+            { name: 'Mar', value: 80 },
+            { name: 'Apr', value: 90 },
+            { name: 'May', value: 60 }
+          ];
+      
 
             this.pieChartData = [
                 { region: 'North', sales: 15 },
                 { region: 'South', sales: 25 },
                 { region: 'East', sales: 10 },
                 { region: 'West', sales: 50 },
-                // ... other data
             ];
         }
     }
